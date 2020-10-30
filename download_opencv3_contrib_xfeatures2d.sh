8688e11d7e2884fa9d50e3eef5c8870e9c1ca4de
JIanzhu Huai <huai.3@osu.edu>
2020-10-30 21:59:12 +0800

    download opencv3 xfeatures2d

---
+++ download_opencv3_contrib_xfeatures2d.sh
@@ -0,0 +1,82 @@
+#!/bin/bash
+# In docker, "catkin make opencv3_catkin" often encounters error
+# "# connect to 151.101.228.133 port 443 failed: Connection refused" in
+# downloading files like boostdesc_bgm_hd.i and vgg_generated_64.i.
+# The current approach is to download them manually and put to the proper folder.
+
+if [[ $# -ne 1 ]]; then
+    echo "Usage: $0 </path/to/maplab_ws>" 
+    exit 1
+fi
+MAPLAB_WS=$1
+if [[ ! -d "$MAPLAB_WS" ]]; then
+  echo "$MAPLAB_WS does not exist!"
+  exit 2 
+fi
+
+VGG_COMMIT="fccf7cd6a4b12079f73bbfb21745f9babcd4eb1d"
+VGG_NAMES=(vgg_generated_48.i \
+vgg_generated_64.i \
+vgg_generated_80.i \
+vgg_generated_120.i)
+
+BOOSTDESC_COMMIT="34e4206aef44d50e6bbcd0ab06354b52e7466d26"
+BOOSTDESC_NAMES=(boostdesc_bgm.i \
+boostdesc_bgm_bi.i \
+boostdesc_bgm_hd.i \
+boostdesc_binboost_064.i \
+boostdesc_binboost_128.i \
+boostdesc_binboost_256.i \
+boostdesc_lbgm.i)
+
+COMPLETED_FILES=0
+
+download_xfeatures2d_file() {
+COMMIT=$1
+FILENAME=$2
+OUTPUT_DIR="$MAPLAB_WS/build/opencv3_catkin/opencv3_src/build/downloads/xfeatures2d"
+FILEPATH="$OUTPUT_DIR/$FILENAME"
+FILEURL="https://raw.githubusercontent.com/opencv/opencv_3rdparty/$COMMIT/$FILENAME"
+if [[ -f "$FILEPATH" ]]; then
+  echo "$FILEPATH exists and saves downloading!"
+  COMPLETED_FILES=$(( $COMPLETED_FILES+1 ))
+else
+  CMD="wget $FILEURL -P $OUTPUT_DIR --connect-timeout=20 --waitretry=0 --retry-connrefused --no-dns-cache"
+  echo $CMD
+  $CMD
+  if [[ $? -ne 0 ]]; then 
+    echo "Failed to download $FILEURL"
+  else 
+    COMPLETED_FILES=$(( $COMPLETED_FILES+1 ))
+  fi
+fi
+}
+
+for (( j = 0; j < ${#VGG_NAMES[@]}; j++ )); do
+  download_xfeatures2d_file $VGG_COMMIT ${VGG_NAMES[$j]}
+done
+
+for (( j = 0; j < ${#BOOSTDESC_NAMES[@]}; j++ )); do
+  download_xfeatures2d_file $BOOSTDESC_COMMIT ${BOOSTDESC_NAMES[$j]}
+done
+
+EXPECTED_FILES=$(( ${#VGG_NAMES[@]}+${#BOOSTDESC_NAMES[@]} ))
+if [[ $COMPLETED_FILES -ne $EXPECTED_FILES ]]; then 
+  echo "Error: Only downloaded $COMPLETED_FILES out of $EXPECTED_FILES vgg and boostdesc files!"
+  exit 1
+else 
+  echo "Finished downloading $EXPECTED_FILES vgg and boostdesc files!"
+  exit 0
+fi
+
+
+# TODO(jhuai): tiny-dnn
+# "https://github.com/tiny-dnn/tiny-dnn/archive/v1.0.0a3.tar.gz" 
+# "/baselines/maplab_bespoke_ws/build/opencv3_catkin/opencv3_src/build/3rdparty/tinydnn"
+
+# TODO(jhuai): Download ippicv
+# DESTINATION_DIR /jhuai/baselines/maplab_bespoke_ws/build/opencv3_catkin/opencv3_src/build/3rdparty/ippicv
+# cmake_download "/baselines/maplab_bespoke_ws/build/opencv3_catkin/opencv3_src/.cache/ippicv/4e0352ce96473837b1d671ce87f17359-ippicv_2017u3_lnx_intel64_general_20170822.tgz" 
+# "https://raw.githubusercontent.com/opencv/opencv_3rdparty/dfe3162c237af211e98b8960018b564bc209261d/ippicv/ippicv_2017u3_lnx_intel64_general_20170822.tgz"
+
+
--- retry.sh
+++ retry.sh
@@ -1,6 +1,18 @@
 #!/bin/bash
 # Build maplab package in a low bandwidth environment.
 # opencv3_catkin and suitesparse are esp. vulnerable to low bandwidth.
+# often times, it is due to the error "Failed to connect to 
+# raw.githubusercontent.com port 443: Connection refused". In this case,
+# try this: open /etc/hosts, append the following line,
+# 199.232.28.133 raw.githubusercontent.com
+# see https://blog.csdn.net/CharlesYooSky/article/details/106354746
+
+# maplab simulation package depends on orocos_kdl, you may need to install it with
+# apt-get update
+# apt-get install ros-kinetic-orocos-kdl
+# If the problem "No rule to make target '/opt/ros/kinetic/lib/liborocos-kdl.so.1.3.0'"
+# persist after installation, you may have to cheat a bit as below,
+# ln -s /opt/ros/kinetic/lib/liborocos-kdl.so.1.3 /opt/ros/kinetic/lib/liborocos-kdl.so.1.3.0
 
 if [[ $# -ne 1 ]]; then
     echo "Usage: $0 </path/to/maplab_ws>" 
@@ -9,34 +21,40 @@ fi
 MAPLAB_WS=$1
 if [[ ! -d "$MAPLAB_WS" ]]; then
   echo "$MAPLAB_WS does not exist!"
-  exit 2 
+  exit 2
 fi
+
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+MAX_ATTEMPTS=5
+
 cd $MAPLAB_WS
+
 CMD="catkin build opencv3_catkin"
 n=0
-until [ $n -ge 20 ]
+until [ $n -ge $MAX_ATTEMPTS ]
 do
    $CMD && break  # substitute your command here
-   rm -rf $MAPLAB_WS/build/opencv3_catkin/*
-   n=$(( $n+1 ))
-   sleep 5
+   $SCRIPT_DIR/download_opencv3_contrib_xfeatures2d.sh $MAPLAB_WS
+   rm -rf $MAPLAB_WS/build/opencv3_catkin/opencv3_src/build/modules/xfeatures2d/*
+   n=$(( $n+1))
+   sleep 1
 done
 
 CMD="catkin build suitesparse"
 n=0
-until [ $n -ge 20 ]
+until [ $n -ge $MAX_ATTEMPTS ]
 do
    $CMD && break  # substitute your command here
    rm -rf $MAPLAB_WS/build/suitesparse/*
    n=$(( $n+1 ))
-   sleep 5
+   sleep 2
 done
 
 CMD="catkin build maplab"
 n=0
-until [ $n -ge 10 ]
+until [ $n -ge $MAX_ATTEMPTS ]
 do
    $CMD && break  # substitute your command here
    n=$(( $n+1))
-   sleep 5
+   sleep 2
 done
